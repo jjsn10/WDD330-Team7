2cd86bd5a6177997c8257f39dd0a4390d4f2dffa
Yujin <ylim.0348@gmail.com>
2026-01-24 13:49:54 -0700

    format

--- src/js/product.js
+++ src/js/product.js
@@ -2,9 +2,9 @@ import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 import { productDetails } from "./productDetails.mjs";
 
-  const productId = getParam("product");
-  
-  async function init() {
+const productId = getParam("product");
+
+async function init() {
   await productDetails(productId);
   console.log(await findProductById(productId));
 }
