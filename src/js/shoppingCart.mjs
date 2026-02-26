fd9fca6c2a6e2760e30343c3fc2c3c8ff6e4f6ed
elorajm <eloraathias@gmail.com>
2026-02-26 16:19:37 -0600

    Netlify

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -29,14 +29,14 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Images.PrimaryMedium}"
+      src="${item.Images?.PrimaryMedium || item.Images?.PrimaryLarge || item.Image || ''}"
       alt="${item.Name}"
     />
   </a>
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__color">${item.Colors?.[0]?.ColorName || ''}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
